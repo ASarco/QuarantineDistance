28748ff5d2c240678de7ee1cb952321702f5fc3a
Ale Sarco <ale@sarco.com.ar>
2020-06-15 17:49:54 +0100

    Swipe to remove invitación a cafecito

--- app/src/main/java/com/sarcobjects/a500mts/CafecitoFragment.kt
+++ app/src/main/java/com/sarcobjects/a500mts/CafecitoFragment.kt
@@ -6,9 +6,8 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.View.OnTouchListener
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
@@ -17,23 +16,33 @@ import kotlinx.android.synthetic.main.cafecito.*
 
 class CafecitoFragment : Fragment() {
 
+    var showCafecito = false;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.cafecito, container, false)
+        val view = inflater.inflate(R.layout.cafecito, container, false)
+        val gestureDetector = GestureDetector(activity, SwipeGestureListener())
+        view.setOnTouchListener(OnTouchListener { v, event ->
+            v.performClick()
+            gestureDetector.onTouchEvent(event)
+            true
+        })
+        return view
     }
 
     override fun onResume() {
         super.onResume()
-
-        fragmentManager!!.beginTransaction().hide(this).commit()
+        if (!showCafecito) {
+            fragmentManager!!.beginTransaction().hide(this).commit()
+        }
         buttonGotoCafecito.setOnClickListener {
             try {
                 val myIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.cafecito_url)))
                 startActivity(myIntent)
-            } catch (e:Exception) {
+            } catch (e: Exception) {
                 Toast.makeText(this.context, getString(R.string.error_no_browser), Toast.LENGTH_SHORT).show()
             }
         }
@@ -48,9 +57,27 @@ class CafecitoFragment : Fragment() {
 
     private val alarmReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            val cafecitoFragment = fragmentManager!!.findFragmentById(R.id.cafecito) as Fragment
-            fragmentManager!!.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).show(cafecitoFragment).commit();
+            showCafecito = true
+            fragmentManager!!.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).show(this@CafecitoFragment).commit();
+        }
+    }
+
+    companion object {
+        val TAG = CafecitoFragment::class.java.simpleName
+        private const val SWIPE_MIN_DISTANCE = 120
+    }
+
+    inner class SwipeGestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+            //Only left to right or right to left
+            if (e1!!.x - e2!!.x > SWIPE_MIN_DISTANCE || e2.x - e1.x > SWIPE_MIN_DISTANCE) {
+                fragmentManager!!.beginTransaction()
+                        //.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                        .hide(this@CafecitoFragment)
+                        .commit()
+            }
+            return true
         }
     }
 }
-
